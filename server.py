7f6996495ddff8424773c97b4bd642ffd17397d5
naser <nasershabbir@gmail.com>
2024-11-09 14:35:41 -0500

    Docker Working

--- server.py
+++ server.py
@@ -1,12 +1,21 @@
-from flask import Flask # type: ignore
+from flask import Flask, render_template, request, url_for, session  # type: ignore
 from pymongo import MongoClient # type: ignore
 
 mongo_client = MongoClient("mongo")
-db = mongo_client["cse312"]
+db = mongo_client["HealthRecords"]
 chat_collection = db["chat"]
 users_collection = db["users"]
 
 app = Flask(__name__)
 
+@app.route('/')
+def home_page():
+    if request.method == 'GET':
+        return render_template('index.html')
+    #yolo
+   
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
+
